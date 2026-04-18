f4e266659df19d01be2bcc6ebddb573cfcd3b7dc
Anson <ansonantony6002@gmail.com>
2026-04-18 14:51:28 +0530

    added if_fiel fiel

---
+++ Python/if-else.py
@@ -0,0 +1,16 @@
+age=int(input("Enter the age "))
+
+if age>=18:
+    print("YOU ARE ELIGIBLE TO VOTE")
+
+else:
+    print("YOU ARE NOT ELIGIBLE TO VOTE")
+
+
+
+
+num=int(input("Enter the number"))
+if num%2==0:
+    print(num,"even number")
+else:
+    print(num,"odd number")
