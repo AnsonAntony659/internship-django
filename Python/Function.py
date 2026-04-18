0fc4130894a81f0087542af8f73d00b715f1b95f
Anson <ansonantony6002@gmail.com>
2026-04-18 14:37:58 +0530

    modified Function fiel

--- Python/Function.py
+++ Python/Function.py
@@ -2,5 +2,12 @@ def function1():
     print("Hello World")
 
 function1()
+
+
 def function2():
     return("Hello how are you  World")
+
+def function3(length,breadth):
+    area=length*breadth
+    return area
+    
