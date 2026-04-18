d05135b146a4edaa05ca83fb747205601a9e707f
Anson <ansonantony6002@gmail.com>
2026-04-18 14:12:01 +0530

    added python folder plus folders

---
+++ Python/Input Method.py
@@ -0,0 +1,12 @@
+name=input("Enter your name ")
+print(" welcome boss",name)
+num_1=input("Enter the First Number")
+num_2=input("Enter the Second Number")
+sum=num_1+num_2
+print(sum)
+num_1=int(input("Enter the First Number"))
+num_2=int(input("Enter the Second Number"))
+sum=num_1+num_2
+print(sum)
+
+
---
+++ Python/program1.py
@@ -0,0 +1,9 @@
+x=3
+y=4
+z=x+y
+z=z+1
+x=y
+y=5
+print("X is:",x)
+print("Y is:",y)
+print("Z is:",z)
