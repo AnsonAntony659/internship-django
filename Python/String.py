61b322407697cc6a8d4ce3efafb5e7cc5ef837cc
Anson <ansonantony6002@gmail.com>
2026-04-20 16:40:40 +0530

    added string and forloop fiel

---
+++ Python/String.py
@@ -0,0 +1,16 @@
+a=" Good Morning"
+print(a)
+
+str=''
+print(str)
+
+c="WE ARE INDIANS   \n WE LOVE OUR COUNTRY"
+print(c)
+
+
+a1=" Name \t Class \t Section"
+print(a1)
+
+
+b="""we are the studentas man we study computer science"""
+print(b)
---
+++ Python/for loop.py
@@ -0,0 +1,52 @@
+for letter in 'python':
+    print(letter)
+
+
+c='python'
+for i in c :
+    print(i)
+
+
+
+
+
+
+
+count=[10,20,30,40,50]
+for c in count:
+    print(c)
+
+
+
+
+numbers=[10,20,30,2,3,4,5,6,7,8,9,]
+for n in numbers:
+    if n%2==0:
+        print(n ,'even number')
+
+
+for i in '1223456':
+    print('welcome to ',i,'times')
+
+
+
+for i in range(2,10,3):
+    print(i)
+
+
+
+i=2
+while(i>=0):
+    j=2
+    while(j>=0):
+        print(2,end='')
+        j=j-1
+    print()
+    i=i-1
+
+for i in range(1,18):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
+
+
