a8550c2d78618be0fb924560aa27a920e475e601
Anson <ansonantony6002@gmail.com>
2026-04-20 14:19:33 +0530

    modified If-Else fiel again

--- Python/if-elif-else.py
+++ Python/if-elif-else.py
@@ -12,14 +12,4 @@ else:
     print("E")
 
 
-salary=int(input("Enter the Slary of a person"))
-if salary <=50000:
-    tax=0.05*salary
-elif salary<=60000:
-    tax=0.07*salary
-elif salary<=70000:
-    tax=0.08*salary
-else :
-    tax=0.10*salary
-    print("salary:",salary,"Tax:",tax)  
-    
+
